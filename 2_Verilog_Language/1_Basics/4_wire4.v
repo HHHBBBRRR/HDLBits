11d141d8fc9d1a818a3ccd9c5ac4b74bb6f5e823
HHHBBBRRR <90436322+HHHBBBRRR@users.noreply.github.com>
2024-01-22 10:39:01 +0800

    Update 4_wire4.v

--- 2_Verilog_Language/1_Basics/4_wire4.v
+++ 2_Verilog_Language/1_Basics/4_wire4.v
@@ -1,7 +1,11 @@
 module top_module( 
-    input in, 
-    output out 
+    input a,b,c,
+    output w,x,y,z 
 );
-    assign out = ~in;
+
+    assign w = a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
     
 endmodule
