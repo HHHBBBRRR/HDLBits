69a7b2d94c210ee454427e5625e32a297c66beac
HHHBBBRRR <90436322+HHHBBBRRR@users.noreply.github.com>
2024-01-30 14:33:24 +0800

    Create 104_examsece241_2014_q7b.v

---
+++ 3 Circuits/3.2 Sequential Logic/3.2.2 Counters/104_examsece241_2014_q7b.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input reset,
+    output OneHertz,
+    output [2:0] c_enable
+); 
+    wire [3:0] Q0,Q1,Q2;
+
+    assign c_enable = {(Q1==4'd9&&Q0==4'd9),(Q0==4'd9),1'b1};
+    assign OneHertz = (Q2==4'd9&&Q1==4'd9&&Q0==4'd9) ? 1'b1 : 1'b0;
+
+    bcdcount counter0 (clk, reset, c_enable[0], Q0);
+    bcdcount counter1 (clk, reset, c_enable[1], Q1);
+    bcdcount counter2 (clk, reset, c_enable[2], Q2);
+    
+endmodule
