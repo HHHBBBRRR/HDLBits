851649939cd762e5b137351b8965daecf0e52fca
HHHBBBRRR <90436322+HHHBBBRRR@users.noreply.github.com>
2024-02-02 11:19:35 +0800

    Create 122_fsm2s.v

---
+++ 3 Circuits/3.2 Sequential Logic/3.2.5 Finite State Machines/122_fsm2s.v
@@ -0,0 +1,39 @@
+module top_module(
+    input clk,
+    input reset,
+    input j,
+    input k,
+    output out
+);  
+    parameter OFF = 0, ON = 1; 
+    reg state, next_state;
+
+    always @(*) begin
+        case (state)
+            ON : if (k) begin
+                next_state = OFF;
+            end
+            else begin
+                next_state = ON;
+            end
+            OFF : if (j) begin
+                next_state = ON;
+            end
+            else begin
+                next_state = OFF;
+            end
+        endcase
+    end
+
+    always @(posedge clk) begin
+        if (reset) begin
+            state <= OFF;
+        end
+        else begin
+            state <= next_state;
+        end
+    end
+
+    assign out = (state == ON) ? 1'b1 : 1'b0;
+
+endmodule
